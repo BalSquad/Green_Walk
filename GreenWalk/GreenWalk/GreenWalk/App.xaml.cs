732f4f45173b9d5ea99db7caf2be3bcc6ac3f2d1
adrien desfarges <a.desfarges@gmail.com>
2018-11-17 00:24:26 +0100

    add appcenter

--- GreenWalk/GreenWalk/GreenWalk/App.xaml.cs
+++ GreenWalk/GreenWalk/GreenWalk/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,7 +19,7 @@ namespace GreenWalk
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("android=c78bca91-89b0-4dd4-babc-4a9c110baf89;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
