fab0ab4aef51cea3c2f41c896a03b48b06b3deb8
Simon SOchacki <razowski57@gmail.com>
2018-11-17 09:24:03 +0100

    Remove endjourney.cs

---
+++ GreenWalk/GreenWalk/GreenWalk/Pages/EndJourney.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GreenWalk.Pages
+{
+    public partial class EndJourney : ContentPage
+    {
+        public EndJourney()
+        {
+            InitializeComponent();
+        }
+    }
+}
