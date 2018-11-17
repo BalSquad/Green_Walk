fbc160d3637c7654cce52a9c16cc0d3e69024b69
adrien desfarges <a.desfarges@gmail.com>
2018-11-17 08:26:13 +0100

    export model

--- GreenWalk/GreenWalkModel/Journey.cs
+++ GreenWalk/GreenWalkModel/Journey.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GreenWalkApi.Models
+namespace GreenWalkModel
 {
     public class Journey
     {
--- GreenWalkApi/Controllers/BinController.cs
+++ GreenWalkApi/Controllers/BinController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GreenWalkApi.Interface;
-using GreenWalkApi.Models;
 using GreenWalkApi.Services;
+using GreenWalkModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GreenWalkApi.Controllers
