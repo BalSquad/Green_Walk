5cbba27119ffe353585d58f5a595d36faa5bf306
Simon SOchacki <razowski57@gmail.com>
2018-11-17 05:20:55 +0100

    Add Services Event Bin and position

--- GreenWalkApi/Services/IBinService.cs
+++ GreenWalkApi/Services/IBinService.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
+using GreenWalkApi.Models;
+
 namespace GreenWalkApi.Services
 {
-        public interface IBinService
-        {            
-                int Get(int id);
-                int GetAll();
-                void Update();
-                void Delete();
-        }
+    public interface IBinService
+    {
+        void Delete(Bin bin);
+        Bin Get(int id);
+        List<Bin> GetAll();
+        void Update(Bin bin);
+
+        void Create(Bin bin);
+    }
 
 }
