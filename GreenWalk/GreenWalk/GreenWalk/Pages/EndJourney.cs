93ecbbb1d16ee9ed032a7c1240b55bff0e0fe053
Simon SOchacki <razowski57@gmail.com>
2018-11-17 08:43:53 +0100

    test screens

---
+++ GreenWalk/GreenWalk/GreenWalk/Pages/EndJourney.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GreenWalk.Pages
+{
+    public class EndJourney : ContentPage
+    {
+        public EndJourney()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
